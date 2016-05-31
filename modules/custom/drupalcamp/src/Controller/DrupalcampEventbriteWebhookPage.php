b085e97dd29797ea45947c0ce297e8f7b5422f9f
Nikolaos Kostopoulos <nkostop@gmail.com>
2016-05-31 23:11:37 +0300

    Controller for eventbrite webhooks initializaed

---
+++ modules/custom/drupalcamp/src/Controller/DrupalcampEventbriteWebhookPage.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nkostop
+ * Date: 25/05/16
+ * Time: 20:28
+ */
+
+namespace Drupal\drupalcamp\Controller;
+
+
+use Drupal\Core\Controller\ControllerBase;
+
+class DrupalcampEventbriteWebhookPage extends ControllerBase
+{
+  public function getPage()
+  {
+    $build = [];
+
+    return $build;
+  }
+}
